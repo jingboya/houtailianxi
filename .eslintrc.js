931a03317678b8ed175952a5bd6442d894074bf0
jingboya <jingboya666@163.com>
2019-08-03 16:51:20 +0800

    自定义ESlint代码校验规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,16 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    // 自定义规则配置对象
+    // key:规则代码
+    // value:规则说明
+    // "off" of 0 -关闭规则，什么都不做
+    // "warn" or 1 -讲规则视为一个警告（不会影响退出码），警告并退出
+    // "error" or 2 -将规则视为一个错误（退出码为1），报错并退出
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // 'semi':['error', 'always']
+    // 'semi':'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
