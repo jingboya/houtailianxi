1d74a449fdb92d273d19f05624a239d34e93c0ee
jingboya <jingboya666@163.com>
2019-08-04 23:13:37 +0800

    使用axios响应拦截器统一处理401状态码

--- src/main.js
+++ src/main.js
@@ -52,12 +52,29 @@ axios.interceptors.request.use(config => {
 axios.interceptors.response.use(response => { // >= 200 && < 400 的状态码进入这里
   // console.log('response => ', response)
   // return response
+
   // 将响应数据处理成统一的数据格式方便使用
   console.log('response => ', response)
-  return response.data.data
+
+  // return response.data.data
+  // 如果返回的数据格式是对象
+  if (typeof response.data === 'object' && response.data.data) {
+    return response.data.data
+  } else {
+    return response.data
+  }
 }, error => { // >= 400 的状态码会进入这里
   // console.dir(error)
-  console.log('response error =>', error)
+  const status = error.response.status
+  if (status === 401) {
+    // 务必删除本地存储中的用户信息数据
+    window.localStorage.removeItem('user_info')
+
+    // 跳转到登录页面
+    router.push({
+      name: 'login'
+    })
+  }
   return Promise.reject(error)
 })
 
