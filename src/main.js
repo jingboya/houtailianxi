d942de871387a7b27e674acb19f584d10f144692
jingboya <jingboya666@163.com>
2019-08-04 22:09:07 +0800

    使用axios请求拦截器统一添加授权

--- src/main.js
+++ src/main.js
@@ -24,6 +24,37 @@ axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 Vue.prototype.$http = axios
 Vue.use(ElementUI)
 
+/**
+ * Axios 请求拦截器
+ * 所有使用 axios 发起的请求都要先经过这里
+ * config 是本次请求相关的配置对象
+ * 我们可以通过修改 config 配置来统一自定义请求相关参数
+ * return config 就是允许通过的方式
+ */
+axios.interceptors.request.use(config => {
+  const userInfo = JSON.parse(window.localStorage.getItem('user_info'))
+  // console.log('request userInfo => ', userInfo)
+  // console.log('request config => ', config)
+
+  // 如果登录了，才给那些需要 token 的接口统一添加 token 令牌
+  // 登录相关接口不需要添加 token 令牌，想要也没有
+  if (userInfo) {
+    config.headers.Authorization = `Bearer ${userInfo.token}`
+  }
+  return config
+}, error => {
+  // console.log('request error', error)
+  return Promise.reject(error)
+})
+/**
+ * Axios响应拦截器
+ */
+axios.interceptors.response.use(response => {
+  return response
+}, error => {
+  return Promise.reject(error)
+})
+
 Vue.config.productionTip = false
 
 new Vue({
