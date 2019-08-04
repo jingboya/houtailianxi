dd16a55108ef5d1e282e9cac9306cc590b5f7a14
jingboya <jingboya666@163.com>
2019-08-04 16:24:34 +0800

    侧边栏组件布局，router 是否使用 vue-router 的模式，启用该模式会在激活导航时以 index 作为 path 进行路由跳转

--- src/views/layout/components/AppAside.vue
+++ src/views/layout/components/AppAside.vue
@@ -1,7 +1,55 @@
 <template>
- <div>
-bian
- </div>
+  <!--
+    el-menu
+      default-active 指定默认激活的菜单项
+      @open、@close 展示和收起菜单项的时候会触发
+      background-color 设置菜单栏的背景色
+      text-color 设置菜单栏文本颜色
+      active-text-color 设置激活时候的高亮颜色
+      router 是否使用 vue-router 的模式，启用该模式会在激活导航时以 index 作为 path 进行路由跳转
+    el-submenu
+      index 导航标识，可以用作路由导航路径，必须唯一
+   -->
+  <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      router
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item index="/">
+        <i class="el-icon-menu"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>内容</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="/publish" class="el-icon-location">发布</el-menu-item>
+          <el-menu-item index="1-2" class="el-icon-location">内容列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-menu-item index="/comment">
+        <i class="el-icon-document"></i>
+        <span slot="title">评论</span>
+      </el-menu-item>
+      <el-menu-item index="/image">
+        <i class="el-icon-setting"></i>
+        <span slot="title">素材</span>
+      </el-menu-item>
+      <el-menu-item index="/fans">
+        <i class="el-icon-setting"></i>
+        <span slot="title">粉丝</span>
+      </el-menu-item>
+      <el-menu-item index="/account">
+        <i class="el-icon-setting"></i>
+        <span slot="title">账户</span>
+      </el-menu-item>
+    </el-menu>
 </template>
 
 <script>
@@ -12,12 +60,22 @@ export default {
 
     }
   },
-  components: {
-
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
   }
 }
 </script>
 
 <style scoped>
-
+  .el-menu {
+    height: 100%;
+  }
+  .iconfont {
+    margin-right: 10px
+  }
 </style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,8 +1,19 @@
 <template>
- <div>
+  <el-container>
+    <el-aside width="200px">
+      <AppAside></AppAside>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <AppHeader></AppHeader>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+  <!-- <div>
    <AppAside></AppAside>
    <AppHeader></AppHeader>
- </div>
+  </div>-->
 </template>
 
 <script>
@@ -11,9 +22,7 @@ import AppHeader from './components/AppHeader'
 export default {
   name: 'AppLayout',
   data () {
-    return {
-
-    }
+    return {}
   },
   components: {
     AppAside,
@@ -22,6 +31,17 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+.el-aside {
+  background-color: #d0dce7;
+}
+.el-header {
+  background-color: #aec0d1;
+}
+.el-main {
+  background-color: #e8eff4;
+}
 </style>
